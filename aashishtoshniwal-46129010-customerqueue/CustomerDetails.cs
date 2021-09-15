f4c25abcd20a96368039c93f461dbc858428ea57
Aashish Toshniwal <aashishtoshniwal16@gmail.com>
2021-09-15 03:53:06 +0000

    Customer queue Project

---
+++ aashishtoshniwal-46129010-customerqueue/CustomerDetails.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aashishtoshniwal_46129010_customerqueue
+{
+    public class CustomerDetails : IEquatable<CustomerDetails>
+    {
+        public string Name { get; set; }
+        public string Email { get; set; } 
+        public long Phone { get;set; }
+
+        public CustomerDetails(string name, string email, long phone)
+        {
+            Name = name;
+            Email = email;
+            Phone = phone;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} {Email} {Phone}";
+        }
+
+
+        public bool Equals(CustomerDetails customerTocompare)
+        {
+            bool isSameCustomer = (Phone == customerTocompare.Phone);
+            return isSameCustomer;
+        }
+    }
+}
---
+++ aashishtoshniwal-46129010-customerqueue/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aashishtoshniwal_46129010_customerqueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string appointmentDone;
+            string name;
+            string email;
+            long phone;
+            bool entryDone=false;
+            Queue<CustomerDetails> customerlist = new Queue<CustomerDetails>();
+            do
+            {
+                Console.WriteLine("Enter Customer Details");
+                Console.Write("Name: ");
+                name = Console.ReadLine();
+                if (name == "done")
+                {
+                    break;
+                }
+                Console.Write("Email: ");
+                email = Console.ReadLine();
+                Console.Write("Phone: ");
+                phone = long.Parse(Console.ReadLine());
+                CustomerDetails cust = new CustomerDetails(name, email, phone);
+                if (customerlist.Contains(cust))
+                {
+                    Console.WriteLine("Customer Already Exists");
+                }
+                else
+                {
+                    customerlist.Enqueue(cust);
+                    Console.WriteLine("Customer Queued");
+                }
+            } while (entryDone!=true);
+
+           /* CustomerDetails customer1 = new CustomerDetails("aashish", "aashish@gmail.com", 8790485829);
+            CustomerDetails customer2 = new CustomerDetails("max", "max@gmail.com", 9701005621);
+            CustomerDetails customer3 = new CustomerDetails("bill", "bill@gmail.com", 879053453);
+            customerlist.Enqueue(customer1);
+            customerlist.Enqueue(customer2);
+            customerlist.Enqueue(customer3);
+            CustomerDetails newcustomer = new CustomerDetails("bill", "bill@gmail.com", 879053453);
+
+            if (customerlist.Contains(newcustomer))
+            {
+                Console.WriteLine("Customer Already Exists");
+            }
+            else
+            {
+                customerlist.Enqueue(newcustomer);
+                Console.WriteLine("Customer Added to the Queue");
+            }
+
+            */
+
+
+            //Check Appointment
+            do
+            {
+                    Console.WriteLine(customerlist.Peek());
+                Console.WriteLine("Appointment Completed?");
+                    appointmentDone = Console.ReadLine();
+                    if (appointmentDone == "Yes")
+                    {
+                        customerlist.Dequeue();
+                    }
+            } while (customerlist.Count != 0);
+            Console.WriteLine("All Appointments done");
+            
+
+        }
+    }
+}
